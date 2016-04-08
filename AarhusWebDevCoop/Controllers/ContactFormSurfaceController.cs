3fa416608a1afd0e1a3a8d7f80d86dfeb355b9e8
Costin Ionel Floricel <costin_floricel@hotmail.com>
2016-04-08 16:23:27 +0200

    First Commit

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,52 @@
+﻿using AarhusWebDevCoop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+
+                TempData["success"] = true;
+                // create message
+                IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Message");
+
+                comment.SetValue("senderName", model.Name);
+                comment.SetValue("email", model.Email);
+                comment.SetValue("subject", model.Subject);
+                comment.SetValue("message", model.Message);
+
+                //Save
+                Services.ContentService.Save(comment);
+
+            }
+            return RedirectToCurrentUmbracoPage();
+        }
+
+    }
+}
---
+++ AarhusWebDevCoop/Models/ContactForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace AarhusWebDevCoop.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "This is not a valid email address!")]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
